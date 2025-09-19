d8ca936d7ecd64d62c8ea7dd467caecae7123ca5
sajan <sajankbastian@gmail.com>
2025-09-19 16:55:32 +0530

    first commit - started library management system.

---
+++ libraryManagementSystem.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+const struct Books{
+    int _id;
+    char title[200];
+    char author[200];
+    int availability;
+};
+
+int main() {
+
+    int selection;
+
+    printf("\nhi, how may i help you ?\nPlease select the option that you want.");
+    printf("\nEnter 1 for Add Books.\nEnter 2 for List books.\nEnter 3 for Search book.\nEnter 4 for Delete book\nEnter 0 to exit\n");
+    scanf("%d", &selection);
+
+    switch(selection) {
+        case 0:
+            printf("exiting from LIBRARY MANAGEMENT SYSTEM");
+            break;
+        case 1:
+            printf("You have selected Add Books");
+            break;
+        case 2:
+            printf("You have selected List books");
+            break;
+        case 3:
+            printf("You have selected List books");
+            break;
+        default:
+            printf("You have entered wrong number,\nplease read the instruction and enter carefully.");
+            break;
+    }
+
+    return 0;
+}
+
+void addBook() {
+    
+}
+
